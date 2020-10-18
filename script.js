82ddc1187e20b5a36edb96686ea4855a755893d1
Stacy Read <sread@ivaalsolutions.com>
2020-10-18 10:24:56 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+$(init);
+
+function init() {
+	// Replace these two lines with jQuery
+	document.getElementById("qtyBook").onchange = updateTotal;
+	document.getElementById("qtyDonation").onchange = updateTotal;
+}
+
+// Fill in this function using jQuery only! Do not use document.getElementById()
+function updateTotal() {
+	// STEP ONE
+	
+		// Declare variables for the book quantity and the donation quantity
+	
+		// Get the values of both quantities from the input boxes ("qtyBook" and "qtyDonation")
+	
+		// Parse the values to number types
+	
+	
+	
+	// STEP TWO
+	
+		// Declare variables for the book unit price and the donation unit price
+	
+		// Get the values of both unit prices from the spans ("priceBook" and "priceDonation")
+	
+		// Parse the values to number types
+	
+	
+	
+	// STEP THREE
+	
+		// Declare variables for the book subtotal and the donation subtotal
+	
+		// Calculate the book subtotal and the donation subtotal, assigning to variables
+	
+		// Update the text of the subtotal spans ("totalBook" and "totalDonation") with the calculated subtotal values
+	
+	
+	
+	// STEP FOUR
+	
+		// Declare a variable for the grand total
+	
+		// Calculate the grand total, assigning it to the variable
+	
+		// Update the text of the grand total span ("grandTotal") with the calculated total
+}
